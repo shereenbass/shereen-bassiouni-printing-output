14efa7e1ea61c1263e4f279cb648fadfb8ccfe77
shereenbass <77513084+shereenbass@users.noreply.github.com>
2021-01-20 16:45:22 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+let a = 3;
+let b = 5;
+let c;
+let first_name = 'Shereen';
+let last_name = 'Bassiouni';
+let email = 'bass0081@algonquinlive.com';
+
+
+//alert("description of alerted_value: " + alerted_value);
+//a + b
+//a - b
+//a * b
+//a / b
+//a % b
+//a += b
+//a -= b+
+//a *= b
+//a /= b
+//a %= b
+//a == b
+//a != b
+//a > b
+//a < b
+//!a && !c 
+//!a || !c 
+
+
+let output = '';
+output += 'let a =3;';
+output += '\n';
+output += 'let b = 5;';
+output += '\n';
+output += 'let c;';
+output += '\n';
+output += '---------';
+output += '\n';
+output += 'a + b =' + (a + b);
+output += '\n';
+output += 'a - b =' + (a - b);
+output += '\n';
+output += 'a * b =' + (a * b);
+output += '\n';
+output += 'a / b =' + (a / b);
+output += '\n';
+output += 'a % b =' + (a % b);
+output += '\n';
+output += '(a += b) =' + (a += b);
+output += '\n';
+output += '(a -= b) =' + (a -= b);
+output += '\n';
+output += '(a *= b) =' + (a *= b);
+output += '\n';
+output += '(a /= b) =' + (a /= b);
+output += '\n';
+output += '(a %= b) =' + (a %= b);
+output += '\n';
+output += '(a != b) =' + (a != b);
+output += '\n';
+output += '(a > b) =' + (a > b);
+output += '\n';
+output += '(a < b) =' + (a < b);
+output += '\n';
+output += '(!a && !c) =' + (!a && !c);
+output += '\n';
+output += '(!a || !c) =' + (!a || !c);
+output += '\n';
+output += `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+alert(output);
+
+
+
